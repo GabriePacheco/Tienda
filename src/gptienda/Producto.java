69f9566b32bb107a1238d6baf6ce7c31f4ab04e2
Gabriel Pacheco <35195070+GabriePacheco@users.noreply.github.com>
2020-06-21 23:39:44 -0500

    Update Producto.java

--- src/gptienda/Producto.java
+++ src/gptienda/Producto.java
@@ -1,18 +1,21 @@
 
+
 package gptienda;
 
+import java.util.Scanner;
 /**
- *
+ *Clase para crear objetos productos
  * @author gpacheco
  */
 public class Producto {
-    
+    // Intancias 
     private int codigo;
     private String nombre;
     private String marca;
     private double precio;
     private int cantidad;
 
+    //constructor Por defecto
     public Producto(int codigo, String nombre, String marca, double precio, int cantidad) {
         this.codigo = codigo;
         this.nombre = nombre;
@@ -20,7 +23,7 @@ public class Producto {
         this.precio = precio;
         this.cantidad = cantidad;
     }
-
+    /*** Getter y Setters **/
     public int getCodigo() {
         return codigo;
     }
@@ -60,23 +63,53 @@ public class Producto {
     public void setCantidad(int cantidad) {
         this.cantidad = cantidad;
     }
-   public void actualizar (int accion, int cantidad){
-       if (accion == 1){
-           this.cantidad = this.cantidad + cantidad;
-            System.out.println("Compra realizada con exito!");
-       }else{
-           if (this.cantidad > cantidad){
-            this.cantidad = this.cantidad - cantidad;   
-            System.out.println("Compra realizada con exito!");
-           }else{
-               System.out.println("La cantidad ingresada es mayor a la del inventario");
-           }
+    
+   //**Metodo actualizar **// 
+   public void actualizar (){//Metodo para realizar la actualización
+       int accion;
+       int cant;
+       Scanner leer = new Scanner(System.in);
+       //Mensaje de que producto se está actualizando
+       System.out.println("\n*******************************");
+       System.out.println("Actualizado producto " + this.getCodigo() + " "+ this.getNombre());
+       System.out.println("*********************************");
+       System.out.println("* 1.- Comprar                   *");
+       System.out.println("* 2.- Vender                    *");
+       System.out.println("*********************************");
+       System.out.println("Escoja una opcion: " );
+       accion = Integer.parseInt(leer.nextLine());//Leer que va a hacer el usuari
+       switch (accion){
+           case 1: // Si el usuario escoje Comprar 
+                   System.out.print("Ingrese la cantidad: "); 
+                   cant = Integer.parseInt(leer.nextLine()); //Solicitar la cantidad
+                    this.cantidad  += cant;
+                   System.out.println("La campra se realizó con exito!");
+                   break; 
+           case 2: //Si el usuario Escoje Vender        
+                   System.out.print("Ingrese la cantidad: "); 
+                   cant = Integer.parseInt(leer.nextLine());// SOliciatar la cantidad
+                   if (this.getCantidad() >=  cant){// Comprovar si existe stoc
+                       this.cantidad  -= cant;//Realizar la venta
+                       System.out.println("La venta se realizó con exito!");
+                   }else{
+                       //Mensaje indicando que no hay suficiente stock
+                       System.out.println("La cantidad ingresada supera al Stock!");
+                   }
+                   break;
+           case 3: // si el usuario Ingresa algo quivocado
+                   System.out.print("El opcion ingresada no es correcta! "); 
+                   break;
        }
    }  
-               
-    @Override
+   
+   public String toLista (){//Metodo que devuelve en string todos los atrobutos concatenados 
+       return "* " + this.getCodigo() + "    "  +  this.getNombre()+ "  " + this.getMarca()+ "  "+
+               this.getPrecio()+ "     " +  this.getCantidad();
+   }
+                
+    @Override// Sobre Escribir metodo toStong
     public String toString (){
         return "Codigo: " + this.getCodigo() + " Nombre: "  + this.getNombre() + " Marca:" 
-                + this.getMarca()  +" Cantidad: " + this.getCantidad();    
+                + this.getMarca()  +" Cantidad: " + this.getCantidad() + " Precio: " + this.getPrecio();    
     }    
 }
